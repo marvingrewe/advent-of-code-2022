a175a3eaefe1bcc8544e6dad521628a2748e36cb
Marvin Grewe <marvin@grewe.org>
2022-12-03 22:33:45 +0100

    convert lambda to reference

--- src/Day02.kt
+++ src/Day02.kt
@@ -29,9 +29,9 @@ fun main() {
         else -> error("tja")
     }
 
-    fun part1(input: List<String>): Int = input.sumOf { row -> matchUpPoints1(row) }
+    fun part1(input: List<String>): Int = input.sumOf(::matchUpPoints1)
 
-    fun part2(input: List<String>): Int = input.sumOf { row -> matchUpPoints2(row) }
+    fun part2(input: List<String>): Int = input.sumOf(::matchUpPoints2)
 
     val testInput = readInput(day + "_test")
 
